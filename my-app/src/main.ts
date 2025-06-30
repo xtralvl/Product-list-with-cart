ea230b0ccb16949cc448e2afb0ceeb49300b4855
Roland Szucs <rolandszucs.private@gmail.com>
2025-06-30 20:05:38 +0200

    button changing and quantity controller

--- my-app/src/main.ts
+++ my-app/src/main.ts
@@ -1,1 +1,52 @@
-import './mobile.scss'
+import './mobile.scss';
+
+const addToCartButtons = document.getElementsByClassName('add-to-cart');
+
+// CHANGES ADD TO CART BUTTON TO INCREMENT BUTTON AND VICA VERSA //
+function buttonChanger() {
+
+    [...addToCartButtons].forEach(addToCartButton => {
+        const addToCartButtonAsHtml = addToCartButton as HTMLElement;
+
+        addToCartButtonAsHtml.addEventListener('click', () => {
+            const container = addToCartButtonAsHtml.closest('.img-and-button-container');
+            const quantityControllerButton = container?.querySelector('.quantity-controller') as HTMLElement;
+            if (!container) {
+                return;
+            };
+            addToCartButtonAsHtml.style.display = 'none';
+            quantityControllerButton.style.display ='flex';
+
+
+            // === QUANTITY CONTROLLER === //
+
+                const quantity = quantityControllerButton.querySelector('.quantity') as HTMLElement;
+                const decrease = quantityControllerButton.querySelector('.decrease');
+                const increase = quantityControllerButton.querySelector('.increase');
+
+                let currentQuantity = 1;
+
+                quantity.innerText = `${currentQuantity}`;
+
+                decrease?.addEventListener('click', () => {
+                    currentQuantity -= 1 ;
+                    quantity.innerText = `${currentQuantity}`;
+
+                    if (currentQuantity < 1) {
+                        addToCartButtonAsHtml.style.display = '';
+                        quantityControllerButton.style.display = '';
+                    }
+                });
+
+                increase?.addEventListener('click', () => {
+                    currentQuantity += 1;
+                    quantity.innerText = `${currentQuantity}`;
+                }); 
+        })
+    })    
+}
+buttonChanger(); 
+
+
+
+
